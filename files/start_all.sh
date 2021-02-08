404ec33ce1bafd8a99a65e82978475c43fe9f963
vdelendik <vdelendik@solvd.com>
2021-02-08 13:34:19 +0300

    #36: removed obsolete chmod operation

--- files/start_all.sh
+++ files/start_all.sh
@@ -48,9 +48,6 @@ adb uninstall io.appium.settings
 adb uninstall io.appium.unlock
 echo "io.appium.* apps uninstalled."
 
-## provide execute permissions to chromedrivers on startup
-#chmod -R a+x $APPIUM_HOME/node_modules/appium-chromedriver/chromedriver/linux
-
 # Note: STF_PROVIDER_... is not a good choice for env variable as STF tries to resolve and provide ... as cmd argument to its service!
 if [ -z "${STF_HOST_PROVIDER}" ]; then
       #STF_HOST_PROVIDER is empty
