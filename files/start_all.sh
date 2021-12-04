4e57ab0d5dc2f556e61b58ae0f2ec43db6a07e75
vdelendik <vdelendik@solvd.com>
2021-12-04 11:59:31 +0300

    #65: reused zebrunner/stf as base container and disabled websockify install and start

--- files/start_all.sh
+++ files/start_all.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-export WEBSOCKIFY_CMD="/opt/websockify/run ${STF_PROVIDER_MAX_PORT} :5900"
+#export WEBSOCKIFY_CMD="/opt/websockify/run ${STF_PROVIDER_MAX_PORT} :5900"
 export SOCKET_PROTOCOL=ws
 export WEB_PROTOCOL=http
 
 if [ -f /opt/nginx/ssl/ssl.crt ] && [ /opt/nginx/ssl/ssl.key ]; then
-    export WEBSOCKIFY_CMD="/opt/websockify/run ${STF_PROVIDER_MAX_PORT} :5900 --ssl-only --cert /opt/nginx/ssl/ssl.crt --key /opt/nginx/ssl/ssl.key"
+#    export WEBSOCKIFY_CMD="/opt/websockify/run ${STF_PROVIDER_MAX_PORT} :5900 --ssl-only --cert /opt/nginx/ssl/ssl.crt --key /opt/nginx/ssl/ssl.key"
     export SOCKET_PROTOCOL=wss
     export WEB_PROTOCOL=https
 fi
 
 # Note: STF_PROVIDER_... is not a good choice for env variable as STF tries to resolve and provide ... as cmd argument to its service!
 if [ -z "${STF_PROVIDER_HOST}" ]; then
-      #STF_PROVIDER_HOST is empty
+      # when STF_PROVIDER_HOST is empty
       STF_PROVIDER_HOST=${STF_PROVIDER_PUBLIC_IP}
 fi
 
@@ -20,7 +20,7 @@ if [ ! -f /usr/bin/java ]; then
   ln -s /usr/lib/jvm/java-8-openjdk-amd64/bin/java /usr/bin/java
 fi
 
-${WEBSOCKIFY_CMD} &
+#${WEBSOCKIFY_CMD} &
 
 stf provider --allow-remote \
         --connect-url-pattern "${STF_PROVIDER_HOST}:<%= publicPort %>" \
@@ -34,9 +34,9 @@ echo "---------------------------------------------------------"
 
 # wait until backgroud processes exists for websockify (python) and node (stf)
 node_pids=`pidof node`
-python_pids=`pidof python`
-
-wait -n $python_pids $node_pids
+#python_pids=`pidof python`
+#wait -n $python_pids $node_pids
+wait -n $node_pids
 
 
 echo "Exit status: $?"
