2bc6ecfdae619119426a047cf2696d83ea4c2d5d
azarouski <azarouski@solvd.com>
2024-01-29 13:34:20 +0100

    'while' loop logic was modified

--- files/start_all.sh
+++ files/start_all.sh
@@ -15,23 +15,29 @@ PUBLIC_IP_PROTOCOL=${PUBLIC_IP_PROTOCOL,,}
 if [[ "$PLATFORM_NAME" == "ios" ]]; then
   # Time variables
   startTime=$(date +%s)
+  # Socket creation status
+  socketCreated=0
   # Parse usbmuxd host and port
   IFS=: read -r USBMUXD_SOCKET_HOST USBMUXD_SOCKET_PORT <<< "$USBMUXD_SOCKET_ADDRESS"
 
   while [ $(( startTime + $USBMUXD_SOCKET_TIMEOUT )) -gt "$(date +%s)" ]; do
-    # start socat client and connect to appium usbmuxd socket
-    rm -f /var/run/usbmuxd
-    socat UNIX-LISTEN:/var/run/usbmuxd,fork,reuseaddr,mode=777 TCP:${USBMUXD_SOCKET_ADDRESS} &
-
     # Check connection
     nc -z -v -w 5 "$USBMUXD_SOCKET_HOST" "$USBMUXD_SOCKET_PORT"
     if [ $? -eq 0 ]; then
+      # start socat client and connect to appium usbmuxd socket
+      rm -f /var/run/usbmuxd
+      socat UNIX-LISTEN:/var/run/usbmuxd,fork,reuseaddr,mode=777 TCP:${USBMUXD_SOCKET_ADDRESS} &
+      if [ $? -eq 0 ]; then socketCreated=1; fi
       break
     else
       echo "Can't establish connection to appium usbmuxd socket [$USBMUXD_SOCKET_ADDRESS], one more attempt in $USBMUXD_SOCKET_PERIOD seconds."
       sleep "$USBMUXD_SOCKET_PERIOD"
     fi
   done
+
+  if [ $socketCreated -eq 0 ]; then
+    exit 1
+  fi
 
   res=$(ios list 2>&1)
   #echo "res: $res"
